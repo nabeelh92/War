2a8511f994315671a954dc90fef0c8baa754c581
nabeelh92 <nabeelhuq@gmail.com>
2017-05-29 20:11:12 -0700

    adding finished Card game

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    // ! is an optional, has a value
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    var rightScore = 0
+
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    var leftScore = 0
+    
+    let cardNames = ["card2", "card3", "card4", "card5", "card6", "card7", "card8",
+                     "card9", "card10", "jack", "queen", "king", "ace"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +33,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(sender: UIButton) {
+        
+        let leftNumber = Int(arc4random_uniform(13))
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
+        
+        let rightNumber = Int(arc4random_uniform(13))
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
+        
+        
+        if leftNumber > rightNumber {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber == rightNumber {
+            
+        }
+        else {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        
+
+    }
 
 }
+
 
